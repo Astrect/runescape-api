fc607be53c5855e22b990f7b28b2285567cacb39
Joshua Filby <joshuaf@shadowscripts.net>
2015-06-10 16:59:06 -0500

    [ci skip] DRY

--- examples/hiscores.js
+++ examples/hiscores.js
@@ -2,57 +2,35 @@
 
 var api = require('../');
 
-/**
- * Get a players RuneScape 3 stats
- */
-api.rs.hiscores.player('zezima').then(function(playerInfo) {
-    var skills = playerInfo.skills,
-        activities = playerInfo.activities;
+function logInfo(info) {
+    var skills = info.skills,
+        activities = info.activities;
 
     console.log(skills);
     console.log(activities);
-}).catch(console.error);
+}
+
+/**
+ * Get a players RuneScape 3 stats
+ */
+api.rs.hiscores.player('zezima').then(logInfo).catch(console.error);
 
 /**
  * Get a players RuneScape 3 stats (ironman)
  */
-api.rs.hiscores.player('sausage', 'ironman').then(function(playerInfo) {
-    var skills = playerInfo.skills,
-        activities = playerInfo.activities;
-
-    console.log(skills);
-    console.log(activities);
-}).catch(console.error);
+api.rs.hiscores.player('sausage', 'ironman').then(logInfo).catch(console.error);
 
 /**
  * Get a players RuneScape 3 stats (hardcore ironman)
  */
-api.rs.hiscores.player('sausage', 'hardcore').then(function(playerInfo) {
-    var skills = playerInfo.skills,
-        activities = playerInfo.activities;
-
-    console.log(skills);
-    console.log(activities);
-}).catch(console.error);
+api.rs.hiscores.player('sausage', 'hardcore').then(logInfo).catch(console.error);
 
 /**
  * Get a players Oldschool RuneScape stats (ironman)
  */
-api.osrs.hiscores.player('lezley', 'ironman').then(function(playerInfo) {
-    var skills = playerInfo.skills,
-        activities = playerInfo.activities;
-
-    console.log(skills);
-    console.log(activities);
-}).catch(console.error);
+api.osrs.hiscores.player('lezley', 'ironman').then(logInfo).catch(console.error);
 
 /**
  * Get a players Oldschool RuneScape stats (ultimate ironman)
  */
-api.osrs.hiscores.player('perm iron', 'ultimate').then(function(playerInfo) {
-    var skills = playerInfo.skills,
-        activities = playerInfo.activities;
-
-    console.log(skills);
-    console.log(activities);
-}).catch(console.error);
+api.osrs.hiscores.player('perm iron', 'ultimate').then(logInfo).catch(console.error);
