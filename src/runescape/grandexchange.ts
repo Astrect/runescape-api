06736fe8cd38bb21502443d03cd8e1c91458fcb5
Austin Paquette <austin@paquette.io>
2020-02-01 17:52:48 -0800

    feat: 🎸 add grand exchange functions

--- src/runescape/grandexchange.ts
+++ src/runescape/grandexchange.ts
@@ -1,6 +1,68 @@
-// Grand Exchange
-export const category = async () => {}
-export const categoryPrices = async () => {}
+import got from "got"
+import { runescape as RSConfigs } from "../configs"
 
-export const item = async () => {}
-export const itemGraph = async () => {}
+// Grand Exchange
+export const category = async (category: number) => {
+  try {
+    const response = await got(RSConfigs.grandexchange.endpoints.category, {
+      searchParams: {
+        category,
+      },
+    }).json()
+
+    return response
+  } catch (error) {
+    console.log(error.response.body)
+    //=> 'Internal server error ...'
+  }
+}
+export const categoryPrices = async (
+  category: number,
+  prefix: string,
+  page: number
+) => {
+  try {
+    const response = await got(
+      RSConfigs.grandexchange.endpoints.categoryPrices,
+      {
+        searchParams: {
+          category,
+          alpha: prefix,
+          page,
+        },
+      }
+    ).json()
+
+    return response
+  } catch (error) {
+    console.log(error.response.body)
+    //=> 'Internal server error ...'
+  }
+}
+
+export const item = async (item: number) => {
+  try {
+    const response = await got(RSConfigs.grandexchange.endpoints.item, {
+      searchParams: {
+        item,
+      },
+    }).json()
+
+    return response
+  } catch (error) {
+    console.log(error.response.body)
+    //=> 'Internal server error ...'
+  }
+}
+export const itemGraph = async (item: number) => {
+  try {
+    const response = await got(
+      `${RSConfigs.grandexchange.endpoints.itemGraph}${item}.json`
+    ).json()
+
+    return response
+  } catch (error) {
+    console.log(error.response.body)
+    //=> 'Internal server error ...'
+  }
+}
