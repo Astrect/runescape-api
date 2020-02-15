f7929c9d56d72269aa2c2be77a08240e3aa26a8c
Austin Paquette <austin@paquette.io>
2020-02-14 20:31:19 -0800

    test: 💍 update tests to reflect code changes

--- src/__tests__/runescape/hiscores.test.ts
+++ src/__tests__/runescape/hiscores.test.ts
@@ -9,16 +9,14 @@ test("Fetch normal player by name (string)", async () => {
 })
 
 test("Fetch ironman player by name (string)", async () => {
-  return hiscores
-    .getPlayer("Iron Soren", { gamemode: "ironman" })
-    .then(data => {
-      expect(data).toBeDefined()
-      expect(data).toBeInstanceOf(Player)
-    })
+  return hiscores.getPlayer("Iron Soren", "ironman").then(data => {
+    expect(data).toBeDefined()
+    expect(data).toBeInstanceOf(Player)
+  })
 })
 
 test("Fetch hardcore player by name (string)", async () => {
-  return hiscores.getPlayer("J a me s", { gamemode: "hardcore" }).then(data => {
+  return hiscores.getPlayer("J a me s", "hardcore").then(data => {
     expect(data).toBeDefined()
     expect(data).toBeInstanceOf(Player)
   })
