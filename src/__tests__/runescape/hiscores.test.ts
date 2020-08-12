8821156483edb41ad517ef8a9236d4180e2521f5
Austin Paquette <austin@paquette.io>
2020-08-12 13:45:33 -0600

    fix tests (#32)

--- src/__tests__/runescape/hiscores.test.ts
+++ src/__tests__/runescape/hiscores.test.ts
@@ -2,7 +2,7 @@ import { Player } from "../../lib/RuneScape"
 import { hiscores } from "../../runescape"
 
 test("Fetch normal player by name (string)", async () => {
-  return hiscores.getPlayer("Paqt").then(data => {
+  return hiscores.getPlayer("Zezima").then(data => {
     expect(data).toBeDefined()
     expect(data).toBeInstanceOf(Player)
   })
